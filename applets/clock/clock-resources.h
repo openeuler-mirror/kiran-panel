135b14042935b676555820f0a38b8be78a80f795
tangjie02 <tangjie02@kylinsec.com.cn>
2021-09-17 09:30:05 +0800

    feature(plugin): Delete fish, clock and notification area plugin.
    - 删除fish, clock和notification area插件
    
    Signed-off-by: tangjie02 <tangjie02@kylinsec.com.cn>

--- applets/clock/clock-resources.h
+++
@@ -1,7 +0,0 @@
-#ifndef __RESOURCE_clock_H__
-#define __RESOURCE_clock_H__
-
-#include <gio/gio.h>
-
-extern GResource *clock_get_resource (void);
-#endif
--- applets/notification_area/na-resources.h
+++
@@ -1,7 +0,0 @@
-#ifndef __RESOURCE_na_H__
-#define __RESOURCE_na_H__
-
-#include <gio/gio.h>
-
-extern GResource *na_get_resource (void);
-#endif
